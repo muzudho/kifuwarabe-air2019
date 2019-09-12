b0ff901dea39a80003b7208ba140d35c86c8c618
むずでょ <muzudho1@gmail.com>
2019-09-12 23:20:18 +0900

    logging

--- kifuwarabe-air2019/src/main.rs
+++ kifuwarabe-air2019/src/main.rs
@@ -1,3 +1,26 @@
+/*
+# - Log level.
+#$env:RUST_LOG = "trace"
+#$env:RUST_LOG = "debug"
+$env:RUST_LOG = "info"
+#$env:RUST_LOG = "warn"
+#$env:RUST_LOG = "error"
+
+# - Log redirect.
+cargo run 2>> ./kifuwarabe-air2019.log
+ */
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    println!("Hello, println!");
+    trace!("Hello, trace!");
+    debug!("Hello, debug!");
+    info!("Hello, info!");
+    warn!("Hello, warn!");
+    error!("Hello, error!");
 }
